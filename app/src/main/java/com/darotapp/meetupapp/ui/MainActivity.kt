e35f2d31b7ee10d75cc3bbf84f257e0cb9202a31
lepea <darotudeen@gmail.com>
2020-02-08 20:59:01 +0100

    created registration form

--- app/src/main/java/com/darotapp/meetupapp/ui/MainActivity.kt
+++ app/src/main/java/com/darotapp/meetupapp/ui/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.darotapp.meetupapp.ui
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.darotapp.meetupapp.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +13,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = Navigation.findNavController(this, R.id.fragment)
+
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//            getWindow().setStatusBarColor(ContextCompat.getColor(getApplicationContext(), R.color.colorAccent));
+//        }
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+
+        return NavigationUI.navigateUp(
+            Navigation.findNavController(this, R.id.fragment), null
+        )
     }
 }
