f746b1d9bf2250ad032ef58eb4e2a9cfae05607c
lepea <darotudeen@gmail.com>
2020-02-08 16:58:24 +0100

    created project

---
+++ app/src/main/java/com/darotapp/meetupapp/ui/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.darotapp.meetupapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.darotapp.meetupapp.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
